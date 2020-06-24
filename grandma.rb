166d0b6c0a8a26426563be569a39031262f40a4c
michelle diehl <michellekadiehl@gmail.com>
2020-06-24 09:34:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,11 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  "I LOVE YOU TOO PUMPKIN!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
+def
 # unless you shout it (type in all capitals).
 
 # If you shout, she can hear you (or at least she thinks so) 
